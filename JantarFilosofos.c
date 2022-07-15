8e89fbaa0e76e29b36f06d3363b4c5c19f3b925a
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2022-07-15 11:59:23 -0300

    Update JantarFilosofos.c

--- JantarFilosofos.c
+++ JantarFilosofos.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define N 5                  // numero de filosofos
-#define LEFT (i + N - 1) % N // numero do vizinho a esquerda de i
-#define RIGHT (i + 1) % N    // numero do vizinho a direita de i
-#define THINKING 0           // pensando
-#define HUNGRY 1             // tentando pegar garfos
-#define EATING 2             // comendo
-#define COND true            // condição
+#define N 5                      // numero de filosofos
+#define ESQUERDA (i + N - 1) % N // numero do vizinho a esquerda de i
+#define DIREITA (i + 1) % N      // numero do vizinho a direita de i
+#define PENSANDO 0               // pensando
+#define COM_FOME 1               // tentando pegar garfos
+#define COMENDO 2                // comendo
+bool cond = true;                // condição
 
 typedef int semaphore;
 
@@ -17,18 +17,10 @@ semaphore s[N];      // semaforo de cada filosofo
 
 void down(semaphore *mutex)
 {
-    if (*mutex == 1)
-    {
-        *mutex = 0;
-    }
 }
 
 void up(semaphore *mutex)
 {
-    if (*mutex == 0)
-    {
-        *mutex = 1;
-    }
 }
 
 /**
@@ -37,7 +29,7 @@ void up(semaphore *mutex)
  */
 void think()
 {
-    state[N] = THINKING;
+    state[N] = PENSANDO;
     printf("Filosofo está pensando.\n");
 }
 
@@ -47,15 +39,15 @@ void think()
  */
 void eat()
 {
-    state[N] = EATING;
+    state[N] = COMENDO;
     printf("Filosofo está comendo.\n");
 }
 
 void test(int i)
 {
-    if (state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    if (state[i] == COM_FOME && state[ESQUERDA] != COMENDO && state[DIREITA] != COMENDO)
     {
-        state[i] = EATING;
+        state[i] = COMENDO;
         up(&s[i]);
     }
 }
@@ -68,7 +60,7 @@ void test(int i)
 void take_forks(int i)
 {
     down(&mutex); // entra na regiao critica
-    state[i] = HUNGRY;
+    state[i] = COM_FOME;
     test(i);     // tenta pegar os garfos
     up(&mutex);  // sai da regiao critica
     down(&s[i]); // bloqueia os garfos que não foram pegos
@@ -83,16 +75,16 @@ void take_forks(int i)
 void put_forks(int i)
 {
     down(&mutex); // entra na regiao critica
-    state[i] = THINKING;
-    test(LEFT);  // ve se o vizinho da esquerda pode comer agora
-    test(RIGHT); // ve se o vizinho da direita pode comer agora
-    up(&mutex);  // sai da regiao critica
+    state[i] = PENSANDO;
+    test(ESQUERDA); // ve se o vizinho da esquerda pode comer agora
+    test(DIREITA);  // ve se o vizinho da direita pode comer agora
+    up(&mutex);     // sai da regiao critica
     printf(" O Filosofo %d devolveu os garfos.\n", i);
 }
 
 void philosopher(int i)
 {
-    while (COND)
+    while (cond)
     {
         think();       // filosofo esta pensando
         take_forks(i); // pega dois garfos ou bloqueia
@@ -103,10 +95,10 @@ void philosopher(int i)
 
 int main(int argc, char const *argv[])
 {
-    for (int i = 1; i <= 5; i++)
+    int i=0;
+    while (i < N)
     {
-        philosopher(i);
+        philosopher(5);
     }
-
     return 0;
 }
