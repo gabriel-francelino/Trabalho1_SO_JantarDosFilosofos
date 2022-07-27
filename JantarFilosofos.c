66567dc5b483f0c0cdc011f4ead907094d0813d5
gabriel-francelino <90209521+gabriel-francelino@users.noreply.github.com>
2022-07-27 10:29:53 -0300

    Update JantarFilosofos.c
    nova solucao usando semphore.h também

--- JantarFilosofos.c
+++ JantarFilosofos.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define N 5                      // numero de filosofos
 #define ESQUERDA (i + N - 1) % N // numero do vizinho a esquerda de i
@@ -11,12 +12,10 @@
 #define FAMINTO 1                // tentando pegar garfos
 #define COMENDO 2                // comendo
 
-typedef pthread_mutex_t semaforo;
-
-int state[N];        // estado do filósofo
-semaforo mutex;      // controla a regiao critica
-semaforo s[N];       // semaforo de cada filosofo
-pthread_t jantar[N]; // Todos os filósofos
+int estado[N]; // estado do filósofo
+sem_t mutex;   // controla a regiao critica
+sem_t s[N];    // semaforo de cada filosofo
+// pthread_t jantar[N];   // Todos os filósofos
 
 void *filosofo(void *param);
 void pegarGarfo(int i);
@@ -26,23 +25,44 @@ void comer(int i);
 void pensar(int i);
 
 /**
+ * @brief Mostra o estado de cada filósofo
+ *
+ */
+void mostrar()
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if (estado[i - 1] == PENSANDO)
+        {
+            printf("O Filosofo %d esta pensando.\n", i);
+        }
+        if (estado[i - 1] == FAMINTO)
+        {
+            printf("O Filosofo %d esta faminto.\n", i);
+        }
+        if (estado[i - 1] == COMENDO)
+        {
+            printf("O Filosofo %d esta comendo.\n", i);
+        }
+    }
+    printf("\n");
+}
+
+/**
  * @brief Realização das ações do filósofo no jantar
  *
  * @param i índice do filósofo
  */
-void *filosofo(void *vparam)
+void *filosofo(void *id)
 {
-    int *i = (int *)(vparam); // Repassa o id do filósofo
-
+    int i = *(int *)id; // Repassa o id do filósofo
     while (true)
     {
-        pensar(*(i));        // filosofo esta pensando
-        pegarGarfo(*(i));    // pega dois garfos ou bloqueia
-        comer(*(i));         // comendo
-        devolverGarfo(*(i)); // devolver os garfos a mesa
+        pensar(i);        // filosofo esta pensando
+        pegarGarfo(i);    // pega dois garfos ou bloqueia
+        comer(i);         // comendo
+        devolverGarfo(i); // devolver os garfos a mesa
     }
-
-    pthread_exit((void *)0); // Legado do retorno
 }
 
 /**
@@ -52,12 +72,12 @@ void *filosofo(void *vparam)
  */
 void pegarGarfo(int i)
 {
-    pthread_mutex_lock(&(mutex)); // entra na regiao critica
-    printf(" O Filosofo %d pegou os garfos.\n", i);
-    state[i] = FAMINTO;
-    testar(i);                      // tenta pegar os garfos
-    pthread_mutex_unlock(&(mutex)); // Sai na região crítica
-    pthread_mutex_lock(&(s[i]));    // Bloqueia os hashis
+    sem_wait(&mutex);    // entra na regiao critica
+    estado[i] = FAMINTO; // altera o estado do filósofo
+    mostrar();
+    testar(i);        // tenta pegar os garfos
+    sem_post(&mutex); // Sai na região crítica
+    sem_wait(&s[i]);  // Bloqueia os hashis
 }
 
 /**
@@ -67,12 +87,12 @@ void pegarGarfo(int i)
  */
 void devolverGarfo(int i)
 {
-    pthread_mutex_lock(&(mutex)); // entra na regiao critica
-    printf(" O Filosofo %d devolveu os garfos.\n", i);
-    state[i] = PENSANDO;
-    testar(ESQUERDA);               // ve se o vizinho da esquerda pode comer agora
-    testar(DIREITA);                // ve se o vizinho da direita pode comer agora
-    pthread_mutex_unlock(&(mutex)); // Sai da regiсo crítica
+    sem_wait(&mutex); // entra na regiao critica
+    estado[i] = PENSANDO;
+    mostrar();
+    testar(ESQUERDA); // ve se o vizinho da esquerda pode comer agora
+    testar(DIREITA);  // ve se o vizinho da direita pode comer agora
+    sem_post(&mutex); // Sai da regiсo crítica
 }
 
 /**
@@ -82,10 +102,11 @@ void devolverGarfo(int i)
  */
 void testar(int i)
 {
-    if (state[i] == FAMINTO && state[ESQUERDA] != COMENDO && state[DIREITA] != COMENDO)
+    if (estado[i] == FAMINTO && estado[ESQUERDA] != COMENDO && estado[DIREITA] != COMENDO)
     {
-        state[i] = COMENDO;
-        pthread_mutex_unlock(&(s[i]));
+        estado[i] = COMENDO;
+        mostrar();
+        sem_post(&s[i]); // libera os garfos
     }
 }
 
@@ -96,10 +117,10 @@ void testar(int i)
  */
 void pensar(int i)
 {
-    int r = (rand() % 10 + 1);
-    state[N] = PENSANDO;
-    printf("\nFilosofo %d esta pensando por %d s.\n", i, r);
-    sleep(r);
+    //int r = (rand() % 10 + 1);
+    float r = 0.001*random();
+    mostrar();
+    usleep((int)r);
 }
 
 /**
@@ -109,43 +130,53 @@ void pensar(int i)
  */
 void comer(int i)
 {
-    int r = (rand() % 10 + 1);
-    state[N] = COMENDO;
-    printf("Filosofo %d esta comendo por %d s.\n", i, r);
-    sleep(r);
+    //int r = (rand() % 10 + 1); // gerar valores de tempo de 1 a 10
+    float r = 0.001*random();
+    mostrar();
+    usleep((int)r);
 }
 
-int main(int argc, char const *argv[])
+void excecao(int e)
 {
-    int cont;   // Contador auxiliar
-    int status; // Criação da thread
-
-    // Inicia os mutexes
-    pthread_mutex_init(&(mutex), NULL);
-    for (cont = 0; cont < N; cont++)
+    if (e != 0)
     {
-        pthread_mutex_init(&(s[cont]), NULL);
+        perror("Erro na inicializacao do semaforo");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        estado[i] = 0;
+    }
+    mostrar();
+    int res;
+    pthread_t thread[N];
+    void *thread_result;
+
+    // Inicia os semaforos
+    res = sem_init(&mutex, 0, 1);
+    excecao(res);
+
+    for (int i = 0; i < N; i++)
+    {
+        res = sem_init(&s[i], 0, 0);
+        excecao(res);
     }
 
-    // Inicia threads (filósofos)
-    for (cont = 0; cont < N; cont++)
+    // cria as threads(filosofos)
+    for (int i = 0; i < N; i++)
     {
-        // verifica se ocorreu erro
-        status = pthread_create(&(jantar[cont]), NULL, filosofo, (void *)&(cont));
-        if (status)
-        {
-            printf("Erro criando thread %d, retornou codigo %d\n", cont, status);
-            return EXIT_FAILURE;
-        }
+        res = pthread_create(&thread[i], NULL, filosofo, &i);
+        excecao(res);
     }
 
-    // Destroi antes de sair
-    pthread_mutex_destroy(&(mutex));
-    for (cont = 0; cont < N; cont++)
+    // faz um join nas threads
+    for (int i = 0; i < N; i++)
     {
-        pthread_mutex_destroy(&(s[cont]));
+        res = pthread_join(thread[i], &thread_result);
+        excecao(res);
     }
-    pthread_exit(NULL);
-
-    return EXIT_SUCCESS;
 }
